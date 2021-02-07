acb0e51dcbc0b9f94e0f53c1e876ea280a78fa1a
souravroy78 <sourav.roy@centrica.com>
2021-02-07 19:55:22 +0000

    Initial commit

---
+++ Chap7Arith2.hs
@@ -0,0 +1,34 @@
+module Chap7Arith2 where
+
+main :: IO ()
+main = do
+    print(0 :: Int)
+    print(add 1 0)
+    print(addOne 0)
+    print(addOnePF 0)
+
+    print((addOne.addOne) 0)
+    print((addOne.addOnePF) 0)
+    print((addOnePF.addOne) 0)
+    print((addOnePF.addOnePF) 0)
+
+    -- this is not function composition
+    print(negate (addOne 0))
+
+    print((negate . addOne) 0)
+    print(negate . addOne $ 0)
+
+    print ((addOne . addOne . addOne . negate . addOne) 0)
+    print (addOne . addOne . addOne . negate . addOne $ 0)
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+addPF :: Int -> Int -> Int
+addPF = (+)
+
+addOne :: Int -> Int
+addOne = \x -> x + 1
+
+addOnePF :: Int -> Int
+addOnePF = (+1)
