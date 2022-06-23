4b771a0ab0576f2558b0836cfeecfb410f83b5ea
Sourav Roy <sourav.roy@hmcts.net>
2022-06-23 22:55:50 +0100

    chapter 4 exercise

---
+++ chap4/Exercise.hs
@@ -0,0 +1,12 @@
+not True && true -- incorrect
+not True && True -- incorrect
+
+not (x = 6) -- incorrect
+not (x == 6) -- correct
+
+(1 * 2) > 5 -- correct
+
+[Merry] > [Happy] -- incorrect
+["Merry"] > ["Happy"] -- correct
+
+[1, 2, 3] ++ "look at me!" -- incorrect, type mismatch
